1fbaef14ce5d460f269fcc29aaa91ecfb5615572
Zenia <zeniavilla@hotmail.com>
2017-08-29 10:18:54 -0700

    Add state with likes to Experience

--- experience-it-app-client/src/components/Experience.js
+++ experience-it-app-client/src/components/Experience.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid, Col } from 'react-bootstrap';
 import Pluralize from 'react-pluralize';
 import { NavLink } from 'react-router-dom';
 
 import '../font-awesome-4.7.0/css/font-awesome.css';
 
-const Experience = props => {
+class Experience extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            likes: 0
+        }
+    }
 
-    const { id, name, location, img_url, likes, liked } = props.experience;
-
-    const backgroundStyle = {
-        backgroundImage: `url(${img_url})`
-    };
-
-    const handleOnClick = props.handleOnClick;
-
+    handleOnClick = event => {
+        event.preventDefault();
+        this.setState({likes: this.state.likes + 1})
+    }
+    
+    // const handleOnClick = props.handleOnClick;
+    render() {
+        const { id, name, location, img_url, likes, liked } = this.props.experience;
+    
+        const backgroundStyle = {
+            backgroundImage: `url(${img_url})`
+        };
+        
     return (
         <div>
             <Col xs={2}>
@@ -23,7 +35,7 @@ const Experience = props => {
                     <Grid className="experience-img-wrapper" style={backgroundStyle} fluid></Grid> 
                     <h2>{name}</h2>
                     <h4>{location}</h4>
-                    <h4><span className="change-icon" onClick={handleOnClick}>
+                    <h4><span className="change-icon">
                         { (liked)
                         ? <i id={id} className="fa fa-heart"></i>
                         : <i id={id} className="fa fa-heart-o"></i>
@@ -31,11 +43,17 @@ const Experience = props => {
                     </span>
                     <Pluralize singular="like" plural="likes" count={likes} />
                     </h4>
+
+
+                    <h4 onClick={ this.handleOnClick}><i id={id} className="fa fa-heart"></i>
+                        {this.state.likes} likes
+                    </h4>
                 </div>
             </NavLink>
             </Col>
         </div>
     )
+}
 }
 
 export default Experience;
