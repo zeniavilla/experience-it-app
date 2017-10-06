00ee73033739954c25a99e6490aadf6003b40975
Zenia <zeniavilla@hotmail.com>
2017-10-06 09:20:59 -0700

    Change Experience Col to md

--- experience-it-app-client/src/components/Experience.js
+++ experience-it-app-client/src/components/Experience.js
@@ -16,25 +16,23 @@ const Experience = props => {
     const handleOnClick = props.handleOnClick;
 
     return (
-        <div>
-            <Col xs={2}>
-            <NavLink to={`/experiences/${id}`}>
-                <div className="experience-card" id={`experience-${id}`} key={id}>
-                    <Grid className="experience-img-wrapper" style={backgroundStyle} fluid></Grid> 
-                    <h2>{name}</h2>
-                    <h4>{location}</h4>
-                    <h4><span className="change-icon" onClick={handleOnClick}>
-                        { (liked)
-                        ? <i id={id} className="fa fa-heart"></i>
-                        : <i id={id} className="fa fa-heart-o"></i>
-                        }
-                    </span>
-                    <Pluralize singular="like" plural="likes" count={likes} />
-                    </h4>
-                </div>
-            </NavLink>
-            </Col>
-        </div>
+        <Col md={2}>
+        <NavLink to={`/experiences/${id}`}>
+            <div className="experience-card" id={`experience-${id}`} key={id}>
+                <Grid className="experience-img-wrapper" style={backgroundStyle} fluid></Grid> 
+                <h2>{name}</h2>
+                <h4>{location}</h4>
+                <h4><span className="change-icon" onClick={handleOnClick}>
+                    { (liked)
+                    ? <i id={id} className="fa fa-heart"></i>
+                    : <i id={id} className="fa fa-heart-o"></i>
+                    }
+                </span>
+                <Pluralize singular="like" plural="likes" count={likes} />
+                </h4>
+            </div>
+        </NavLink>
+        </Col>
     )
 }
 
